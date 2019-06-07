39c3a987f4622532aa778f86ad022f2727e67d34
Benjamin Day <benday@benday.com>
2019-06-07 17:41:37 +0000

    Added ability to dynamically add includes to queries.

--- Benday.EfCore.SqlServer/Benday.EfCore.SqlServer/SqlEntityFrameworkCrudRepositoryBase.cs
+++ Benday.EfCore.SqlServer/Benday.EfCore.SqlServer/SqlEntityFrameworkCrudRepositoryBase.cs
@@ -39,18 +39,51 @@ namespace Benday.EfCore.SqlServer
             Context.SaveChanges();
         }
 
+        protected virtual List<string> Includes
+        {
+            get;
+        }
+
         public virtual IList<TEntity> GetAll()
         {
-            return EntityDbSet.ToList();
+            var queryable = EntityDbSet.AsQueryable();
+
+            queryable = AddIncludes(queryable);
+
+            return queryable.ToList();
+        }
+
+        protected virtual IQueryable<TEntity> AddIncludes(IQueryable<TEntity> queryable)
+        {
+            if (queryable == null)
+            {
+                throw new ArgumentNullException(nameof(queryable));
+            }
+
+            if (Includes == null || Includes.Count == 0)
+            {
+                return queryable;
+            }
+            else
+            {
+                foreach (var item in Includes)
+                {
+                    queryable = queryable.Include(item);
+                }
+
+                return queryable;
+            }
         }
 
         public virtual TEntity GetById(int id)
         {
-            return (
-                from temp in EntityDbSet
-                where temp.Id == id
-                select temp
-                ).FirstOrDefault();
+            var query = from temp in EntityDbSet
+                        where temp.Id == id
+                        select temp;
+
+            query = AddIncludes(query);
+
+            return query.FirstOrDefault();
         }
 
         public virtual void Save(TEntity saveThis)
