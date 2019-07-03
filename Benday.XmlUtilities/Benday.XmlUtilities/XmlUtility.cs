a9048fcecf53d28c9a38eca93e5088fb4a63405e
Benjamin Day <benday@benday.com>
2019-07-03 10:40:12 -0400

    Added xml utilities project and code.

---
+++ Benday.XmlUtilities/Benday.XmlUtilities/XmlUtility.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Benday.XmlUtilities
+{
+    public static class XmlUtility
+    {
+        public static double GetAttributeValueAsDouble(XElement element, string attributeName)
+        {
+            var value = GetAttributeValue(element, attributeName);
+
+            if (String.IsNullOrEmpty(value) == true)
+            {
+                return (double)0;
+            }
+            else
+            {
+                double temp = default(double);
+
+                if (Double.TryParse(value, out temp) == true)
+                {
+                    return temp;
+                }
+                else
+                {
+                    return temp;
+                }
+            }
+        }
+
+        public static string GetAttributeValue(XElement element, string attributeName)
+        {
+            if (element == null)
+                throw new ArgumentNullException("fromValue", "fromValue is null.");
+            if (String.IsNullOrEmpty(attributeName))
+                throw new ArgumentException("attributeName is null or empty.", "attributeName");
+
+
+            if (element.Attribute(attributeName) != null)
+            {
+                return element.Attribute(attributeName).Value;
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
+        public static void SetAttributeValue(XElement toValue, string name, double value)
+        {
+            SetAttributeValue(toValue, name, value.ToString());
+        }
+
+        public static void SetAttributeValue(XElement toValue, string name, string value)
+        {
+            toValue.SetAttributeValue(name, value);
+        }
+
+        public static string GetAttributeValue(XElement element, string attributeName, string defaultReturnValue)
+        {
+            if (element == null)
+                return defaultReturnValue;
+            else
+            {
+                var value = element.Attribute(attributeName);
+                if (value == null)
+                    return defaultReturnValue;
+                else
+                    return value.Value;
+            }
+        }
+
+        public static void SetChildElement(XElement parentElement,
+            string childElementName,
+            double childElementValue)
+        {
+            SetChildElement(parentElement, childElementName,
+                childElementValue.ToString());
+        }
+
+        public static void SetChildElement(XElement parentElement,
+            string childElementName,
+            DateTime childElementValue)
+        {
+            SetChildElement(parentElement, childElementName,
+                childElementValue.ToString());
+        }
+
+        public static void SetChildElement(XElement parentElement, string childElementName, string childElementValue)
+        {
+            if (parentElement == null)
+                throw new ArgumentNullException("parentElement", "parentElement is null.");
+            if (String.IsNullOrEmpty(childElementName))
+                throw new ArgumentException("childElementName is null or empty.", "childElementName");
+            if (String.IsNullOrEmpty(childElementValue))
+            {
+                childElementValue = String.Empty;
+            }
+
+            parentElement.SetElementValue(childElementName, childElementValue);
+        }
+
+        public static XElement GetChildElement(XElement parentElement, string childElementName)
+        {
+            if (parentElement == null)
+                throw new ArgumentNullException("parentElement", "parentElement is null.");
+            if (String.IsNullOrEmpty(childElementName))
+                throw new ArgumentException("childElementName is null or empty.", "childElementName");
+
+            var childElement = parentElement.Descendants(childElementName).FirstOrDefault();
+            return childElement;
+        }
+
+        public static string GetChildElementValue(XElement parentElement,
+            string childElementName,
+            string defaultReturnValue)
+        {
+            if (parentElement == null)
+                throw new ArgumentNullException("parentElement", "parentElement is null.");
+            if (String.IsNullOrEmpty(childElementName))
+                throw new ArgumentException("childElementName is null or empty.", "childElementName");
+
+            XElement childElement = GetChildElement(parentElement, childElementName);
+
+            if (childElement == null)
+                return defaultReturnValue;
+            else
+                return childElement.Value;
+        }
+
+        public static DateTime GetChildElementValue(XElement parentElement,
+            string childElementName,
+            DateTime defaultReturnValue)
+        {
+            if (parentElement == null)
+                throw new ArgumentNullException("parentElement", "parentElement is null.");
+            if (String.IsNullOrEmpty(childElementName))
+                throw new ArgumentException("childElementName is null or empty.", "childElementName");
+
+            XElement childElement = GetChildElement(parentElement, childElementName);
+
+            if (childElement == null)
+                return defaultReturnValue;
+            else
+            {
+                DateTime returnValue;
+
+                if (DateTime.TryParse(childElement.Value, out returnValue) == true)
+                {
+                    return returnValue;
+                }
+                else
+                {
+                    return defaultReturnValue;
+                }
+            }
+        }
+
+        public static int GetChildElementValue(XElement parentElement,
+            string childElementName,
+            int defaultReturnValue)
+        {
+            if (parentElement == null)
+                throw new ArgumentNullException("parentElement", "parentElement is null.");
+            if (String.IsNullOrEmpty(childElementName))
+                throw new ArgumentException("childElementName is null or empty.", "childElementName");
+
+            XElement childElement = GetChildElement(parentElement, childElementName);
+
+            if (childElement == null)
+                return defaultReturnValue;
+            else
+            {
+                int returnValue;
+
+                if (Int32.TryParse(childElement.Value, out returnValue) == true)
+                {
+                    return returnValue;
+                }
+                else
+                {
+                    return defaultReturnValue;
+                }
+            }
+        }
+
+        public static string GetChildElementValue(XElement parentElement,
+            string childElementName,
+            string childAttributeName,
+            string childAttributeValue,
+            string defaultReturnValue)
+        {
+            if (parentElement == null)
+                throw new ArgumentNullException("parentElement", "parentElement is null.");
+            if (String.IsNullOrEmpty(childElementName))
+                throw new ArgumentException("childElementName is null or empty.", "childElementName");
+
+            var childElement = (
+                from temp in parentElement.Descendants(childElementName)
+                where temp.HasAttributes == true &&
+                temp.Attribute(childAttributeName) != null &&
+                temp.Attribute(childAttributeName).Value == childAttributeValue
+                select temp).FirstOrDefault();
+
+            if (childElement == null)
+                return defaultReturnValue;
+            else
+                return childElement.Value;
+        }
+
+        public static XDocument StringToXDocument(string fromValue)
+        {
+            return XDocument.Parse(fromValue);
+        }
+    }
+}
---
+++ Benday.XmlUtilities/Benday.XmlUtilities/XmlUtilityExtensionMethods.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Benday.XmlUtilities
+{
+    public static class XmlUtilityExtensionMethods
+    {
+        public static IEnumerable<XElement> ElementsByLocalName(this XElement parent, string name)
+        {
+            if (parent == null)
+            {
+                return null;
+            }
+            else
+            {
+                var result = (from temp in parent.Elements()
+                              where temp.Name.LocalName == name
+                              select temp);
+
+                return result;
+            }
+        }
+
+        public static IEnumerable<XElement> ElementsByLocalNameAndAttributeValue(this XElement parent,
+            string elementName,
+            string attributeName,
+            string attributeValue)
+        {
+            var matchingElementsByName = parent.ElementsByLocalName(elementName);
+
+            var result = (from temp in matchingElementsByName
+                          where
+                          temp.HasAttributes == true &&
+                          temp.AttributeValue(attributeName) == attributeValue
+                          select temp);
+
+            return result;
+        }
+
+        public static XElement ElementByLocalName(this XElement parent, string name)
+        {
+            if (parent == null)
+            {
+                return null;
+            }
+            else
+            {
+                var result = (from temp in parent.Elements()
+                              where temp.Name.LocalName == name
+                              select temp).FirstOrDefault();
+
+                return result;
+            }
+        }
+
+        public static XElement ElementByLocalNameAndAttributeValue(this XElement parent,
+            string elementName,
+            string attributeName,
+            string attributeValue)
+        {
+            var matchingElementsByName = parent.ElementsByLocalName(elementName);
+
+            var match = (from temp in matchingElementsByName
+                         where
+                         temp.HasAttributes == true &&
+                         temp.AttributeValue(attributeName) == attributeValue
+                         select temp).FirstOrDefault();
+
+            return match;
+        }
+
+        public static XElement ElementByLocalNameAndAttributeValue(this XElement parent,
+            string elementName,
+            string attributeName1,
+            string attributeValue1,
+            string attributeName2,
+            string attributeValue2)
+        {
+            var matchingElementsByName = parent.ElementsByLocalName(elementName);
+
+            var match = (from temp in matchingElementsByName
+                         where
+                         temp.HasAttributes == true &&
+                         temp.AttributeValue(attributeName1) == attributeValue1 &&
+                         temp.AttributeValue(attributeName2) == attributeValue2
+                         select temp).FirstOrDefault();
+
+            return match;
+        }
+
+        /// <summary>
+        /// Finds a child element starting from parent and returns the inner text value.
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="childElement"></param>
+        /// <returns></returns>
+        public static string ElementValue(this XElement parent, string childElement)
+        {
+            var child = parent.ElementByLocalName(childElement);
+
+            if (child == null)
+            {
+                return null;
+            }
+            else
+            {
+                return child.Value;
+            }
+        }
+
+        public static void SetElementValueByLocalName(this XElement parent, string childElement, string value)
+        {
+            var child = parent.ElementByLocalName(childElement);
+
+            if (child == null)
+            {
+                throw new InvalidOperationException("Could not locate target element.");
+            }
+            else
+            {
+                child.Value = value;
+            }
+        }
+
+        public static string ElementValueByChildNameAndAttributeValue(
+            this XElement parent, string childElement,
+            string attributeName, string attributeValue)
+        {
+            var child = parent.ElementByLocalNameAndAttributeValue(childElement, attributeName, attributeValue);
+
+            if (child == null)
+            {
+                return null;
+            }
+            else
+            {
+                return child.Value;
+            }
+        }
+
+        public static string AttributeValue(this XElement parent, string attributeName)
+        {
+            if (parent == null)
+            {
+                return String.Empty;
+            }
+            else if (parent.HasAttributes == false)
+            {
+                return String.Empty;
+            }
+            else if (parent.Attribute(attributeName) == null)
+            {
+                return String.Empty;
+            }
+            else
+            {
+                return parent.Attribute(attributeName).Value;
+            }
+        }
+    }
+}
