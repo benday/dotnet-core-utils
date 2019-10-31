de6171d9e7b88b070d8421970cb8694cd9e85f7f
Benjamin Day <benday@benday.com>
2019-10-31 14:38:59 -0400

    Added search result.

---
+++ Benday.Common/Benday.Common/SearchResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Benday.Common
+{
+    public class SearchResult<T>
+    {
+        public virtual IList<T> Results { get; set; }
+        public virtual Search SearchRequest { get; set; }
+    }
+}
